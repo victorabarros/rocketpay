615789e0e8e914793494e63f0221834aa0a68457
VictorBarros <victorbarros1130@gmail.com>
2021-02-24 23:05:45 -0300

    sum numbers from file POC

---
+++ rocketpay/lib/rocketpay/numbers.ex
@@ -0,0 +1,19 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(fn ii -> String.to_integer(ii) end)
+      |> Enum.sum()
+
+      {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+end
